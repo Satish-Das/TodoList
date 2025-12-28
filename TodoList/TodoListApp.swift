fc8b6186b69674b05f88afd2238b9afe68a21523
Satish Das <dasskumar9801@gmail.com>
2025-12-28 18:09:33 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Satish Das on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
