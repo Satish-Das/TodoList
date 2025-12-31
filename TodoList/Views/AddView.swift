e248b8bf80129e6863496c1fda8b999fa55f2fdb
Satish Das <dasskumar9801@gmail.com>
2025-12-31 15:13:09 +0530

    Implement AddView with item validation and alert handling

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct AddView : View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
     var body: some View {
         ScrollView{
             VStack{
@@ -18,9 +25,7 @@ struct AddView : View {
                     .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
                     .cornerRadius(10)
                 
-                    Button(action: {
-                        
-                    }, label: {
+                    Button(action: saveButtonPressed, label: {
                         Text("Save".uppercased())
                             .foregroundColor(.white)
                             .font(.headline)
@@ -33,6 +38,26 @@ struct AddView : View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖋️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    func saveButtonPressed(){
+        if textIsAppropriate(){
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss() // this line for go back for one time after save.
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long!!!"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -41,5 +66,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView{
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
